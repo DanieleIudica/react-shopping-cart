1b11270f012049cc2479b02714d6ac11b6dbded0
danieleiudica <daniele.iudica@hotmail.it>
2023-10-08 18:13:07 +0200

    wip

--- shopping-cart-react/src/pages/Store.tsx
+++ shopping-cart-react/src/pages/Store.tsx
@@ -1,3 +1,10 @@
+import { Row } from "react-bootstrap";
+
 export const Store = () => {
-  return <div>Store</div>;
+  return (
+    <>
+      <h1>Store</h1>
+      <Row lg={3} md={2} xs={1}></Row>
+    </>
+  );
 };
